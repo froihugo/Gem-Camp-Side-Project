eeee5a306db2c2ea3aeb69e456727a69afc7cc65
froihugo <jayfhugo@gmail.com>
2022-12-02 17:56:19 +0800

    add default country for phone

---
+++ config/initializers/phonelib.rb
@@ -0,0 +1,6 @@
+Phonelib.default_country = "PH"
+Phonelib.strict_check = true
+Phonelib.ignore_plus = true
+Phonelib.sanitize_regex = '[\.\-\(\) \;\+]'
+Phonelib.extension_separator = ';'
+Phonelib.extension_separate_symbols = %w(ext # ; extension) # each string will be treated as separator
