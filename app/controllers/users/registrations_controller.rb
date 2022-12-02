255b0f384f53265e5526645495d381c01972aa8a
froihugo <jayfhugo@gmail.com>
2022-12-02 17:41:35 +0800

    done username

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters
 
   # GET /resource/sign_up
   # def new
@@ -59,4 +60,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  #
+      protected
+      def configure_permitted_parameters
+        attr_update = [:username, :email, :password, :password_confirmation, :remember_me]
+        devise_parameter_sanitizer.permit :account_update, keys: attr_update
+        attr_sign_up = [:username, :email, :password, :password_confirmation, :remember_me]
+        devise_parameter_sanitizer.permit :sign_up, keys: attr_sign_up
+      end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, :with => /^[A-Za-z\d ]*$/, :multiline => true
+
 end
