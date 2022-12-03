fc6c371a480e8e18d7bc4e5762c78e800f43583f
froihugo <jayfhugo@gmail.com>
2022-12-04 00:21:32 +0800

    add migration add avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_101137) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_080919) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_101137) do
     t.string "username"
     t.string "phone"
     t.integer "role", default: 0
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["phone"], name: "index_users_on_phone"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
