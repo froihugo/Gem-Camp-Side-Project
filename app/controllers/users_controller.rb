d777881710cade684b2feb09242fea119098807a
froihugo <jayfhugo@gmail.com>
2022-12-08 00:41:37 +0800

    change show action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!, except: [:show]
-
     def show
-        @user = current_user
-        @user = User.find_by_username(params[:username])
-    end
-          
-    def user_params
-        params.require(:user).permit(:avatar, :phone, :username, :email)
+        @user = User.find_by_id(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             :controllers => {
-               :registrations => 'users/registrations',
-               :sessions => 'users/sessions',
-             }
-
-  resources :users, only: :show, :path => :username
-  match '/:username' => 'users#show', :via => :get, as: 'profile'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
-
-  constraints(ClientDomainConstraint.new) do
+constraints(ClientDomainConstraint.new) do
+    root "home#index"
+    devise_for :users, :controllers => { :registrations => 'users/registrations', :sessions => 'users/sessions' }
+    resource :users, only: [:show]
+    get 'users/:id' => 'users#show', as: :user
   end
 
   constraints(AdminDomainConstraint.new) do
